05c34b4744ae5c8b110a21f598a6e76adbecfea2
Edi <edi.sinovcic@gmail.com>
2020-04-13 16:00:20 +0200

    Updating deployed stats

--- prepare.sh
+++ prepare.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-
+source .env
 mkdir -p $REPOSITORY_PATH
-mkdir -p $DATABASE_PATH
-mkdir -p $DATABASE_PATH/mainnet
-mkdir -p $DATABASE_PATH/goerli
-mkdir -p $DATABASE_PATH/rinkeby
+mkdir -p $DATABASE_PATH/mainnet/geth
+mkdir -p $DATABASE_PATH/goerli/geth
+mkdir -p $DATABASE_PATH/rinkeby/geth
+mkdir -p $DATABASE_PATH/mainnet/chaindb
+mkdir -p $DATABASE_PATH/goerli/chaindb
+mkdir -p $DATABASE_PATH/rinkeby/chaindb
